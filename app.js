5595e1a3578e5f9f0041941974f6d44099489674
LGou-full <legiostridesign@gmail.com>
2024-09-18 21:16:10 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,6 @@
+document
+  .querySelector(".menu-container")
+  .addEventListener("click", function () {
+    document.querySelector(".sidebar").classList.toggle("active");
+    document.querySelector(".menu-list").classList.toggle("active");
+  });
